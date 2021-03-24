c28bb209d9b1b5f55bb88661afe631303a9bc61f
Noel <Noel@Noel>
2021-03-23 23:54:56 -0600

    Se agregan nuevas funciones para submenus Revisar archivos default.css, funciones.js y plantilla para tener referencia de como implementar todos los submenus

--- src/main/webapp/resources/script/funciones.js
+++ src/main/webapp/resources/script/funciones.js
@@ -2,20 +2,33 @@ window.onload = iniciarDatos;
 
 function iniciarDatos() {
     document.getElementById("open").onclick = openNav;
-    document.getElementById("sidenavBar").onclick = closeNav;
+    document.getElementById("close").onclick = closeNav;
 }
 
 function openNav(){
     document.getElementById("sidenavBar").style.width = "200px";
-    document.getElementById("top").style.marginleft = "200px";
-    document.getElementById("left").style.marginleft = "200px";
-    document.getElementById("content").style.marginleft= "200px";
 }
 
 function closeNav(){
     document.getElementById("sidenavBar").style.width = "0";
-    document.getElementById("top").style.marginleft = "0";
-    document.getElementById("left").style.marginleft = "0";
-    document.getElementById("content").style.marginleft = "0";
 }
 
+/* Funcion para abrir y cerra el menu cuando se da click sobre el  */
+function abrirDropMenu() {
+  document.getElementById("despMenu").classList.toggle("mostrar");
+}
+
+// Cerrar el menu cuando el usuario hace click fuera de el
+window.onclick = function(event) {
+  if (!event.target.matches('.droplink')) {
+    var dropMenu = document.getElementsByClassName("mostrar-contenido");
+    var i;
+    for (i = 0; i < dropMenu.length; i++) {
+      var openDropdown = dropMenu[i];
+      if (openDropdown.classList.contains('mostrar')) {
+        openDropdown.classList.remove('mostrar');
+        document.getElementById("gestEmpleado").style.backgroundcolor="#730602";
+      }
+    }
+  }
+}
