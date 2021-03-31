96afa07cf8b06295bcd0f8e70888a5c524b9b3e6
Noel <Noel@Noel>
2021-03-31 00:39:33 -0600

    Agregados submenus, revisar plantilla para tomar como referencia y completar los que faltan Modificados default.css, funciones, plantilla y pom

--- src/main/webapp/resources/script/funciones.js
+++ src/main/webapp/resources/script/funciones.js
@@ -1,34 +1,48 @@
 window.onload = iniciarDatos;
 
 function iniciarDatos() {
-    document.getElementById("open").onclick = openNav;
-    document.getElementById("close").onclick = closeNav;
+    //document.getElementById("open").onclick = openNav;
+    //document.getElementById("close").onclick = closeNav;
 }
 
-function openNav(){
+function openNav() {
     document.getElementById("sidenavBar").style.width = "200px";
 }
 
-function closeNav(){
+function closeNav() {
     document.getElementById("sidenavBar").style.width = "0";
 }
 
-/* Funcion para abrir y cerra el menu cuando se da click sobre el  */
+/* Funcion para abrir y cerra el menu cuando se da click sobre los links <a> */
 function abrirDropMenu() {
-  document.getElementById("despMenu").classList.toggle("mostrar");
+    var emp = document.getElementById("gestProyectoE");
+    var cli = document.getElementById("gestProyectoC");
+    var art = document.getElementById("gestProyectoA");
+
+    emp.onclick = function () {
+        document.getElementById("despMenuE").classList.toggle("mostrar");
+    };
+
+    cli.onclick = function () {
+        document.getElementById("despMenuC").classList.toggle("mostrar");
+    };
+
+    art.onclick = function () {
+        document.getElementById("despMenuA").classList.toggle("mostrar");
+    }
 }
 
 // Cerrar el menu cuando el usuario hace click fuera de el
-window.onclick = function(event) {
-  if (!event.target.matches('.droplink')) {
-    var dropMenu = document.getElementsByClassName("mostrar-contenido");
-    var i;
-    for (i = 0; i < dropMenu.length; i++) {
-      var openDropdown = dropMenu[i];
-      if (openDropdown.classList.contains('mostrar')) {
-        openDropdown.classList.remove('mostrar');
-        document.getElementById("gestEmpleado").style.backgroundcolor="#730602";
-      }
+window.onclick = function (event) {
+    if (!event.target.matches('.droplink')) {
+        var dropMenu = document.getElementsByClassName("mostrar-contenido");
+        var i;
+        for (i = 0; i < dropMenu.length; i++) {
+            var openDropdown = dropMenu[i];
+            if (openDropdown.classList.contains('mostrar')) {
+                openDropdown.classList.remove('mostrar');
+                document.getElementById("despMenuE").style.backgroundcolor = "white";
+            }
+        }
     }
-  }
-}
+};
